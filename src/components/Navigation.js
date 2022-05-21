94e48ca5bb0726493fc235b71a94839d31763bdd
Austin Jacobs <aust.t.jac@gmail.com>
2022-05-20 23:58:55 -0600

    push

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Grid from '../components/styles/Grid';
+import Flex from '../components/styles/Flex';
 import Box from '../components/styles/Box';
-import { Text } from '@nextui-org/react';
+import { Text, Link, Avatar } from '@nextui-org/react';
 import { useTheme as useNextTheme } from 'next-themes';
 import { Switch, useTheme } from '@nextui-org/react';
 
 function Navigation() {
-const { setTheme } = useNextTheme();
-const { isDark } = useTheme();
+  const { setTheme } = useNextTheme();
+  const { isDark } = useTheme();
 
   return (
     // <div className='navigation'>
@@ -46,11 +47,50 @@ const { isDark } = useTheme();
     //   </nav>
     // </div>
     <React.Fragment>
-      NAVIGATION
-      <Switch
-        checked={isDark}
-        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
-      />
+        <Grid m='1em' gridTemplateColumns='50px 20px 1fr 1fr 1fr 1fr'>
+          <Grid gridColumn='1'>
+            <Flex justifySelf='center' alignSelf='center'>
+              <Avatar text='AJ' size='md' />
+            </Flex>
+          </Grid>
+          <Grid gridColumn='3'>
+            <Flex justifySelf='center' alignSelf='center'>
+              <Link block color='text' href='/'>
+                Home
+              </Link>
+            </Flex>
+          </Grid>
+          <Grid gridColumn='4'>
+            <Flex justifySelf='center' alignSelf='center'>
+              <Link block color='text' href='/about'>
+                About
+              </Link>
+            </Flex>
+          </Grid>
+          <Grid gridColumn='5'>
+            <Flex justifySelf='center' alignSelf='center'>
+              <Link block color='text' href='/experience'>
+                Experience
+              </Link>
+            </Flex>
+          </Grid>
+          <Grid gridColumn='6'>
+            <Flex justifySelf='center' alignSelf='center'>
+              <Link block color='text' href='/work'>
+                Work
+              </Link>
+            </Flex>
+          </Grid>
+          <Grid gridColumn='6'>
+            <Flex justifySelf='right' alignSelf='center'>
+              <Switch
+                size='sm'
+                checked={isDark}
+                onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+              />
+            </Flex>
+          </Grid>
+        </Grid>
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navigation, Footer, Home, About, Contact } from './components';
 import reportWebVitals from './reportWebVitals';
@@ -13,29 +12,8 @@ const lightTheme = createTheme({
   type: 'light',
   theme: {
     colors: {
-      // brand colors
-      primaryLight: '$green200',
-      primaryLightHover: '$green300',
-      primaryLightActive: '$green400',
-      primaryLightContrast: '$green600',
-      primary: '#4ADE7B',
-      primaryBorder: '$green500',
-      primaryBorderHover: '$green600',
-      primarySolidHover: '$green700',
-      primarySolidContrast: '$white',
-      primaryShadow: '$green500',
-
-      gradient:
-        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
-      link: '#5E1DAD',
-
-      // you can also create your own color
       myColor: '#ff4ecd',
-
-      // ...  more colors
-    },
-    space: {},
-    fonts: {},
+    }
   }, // optional
 });
 
@@ -43,29 +21,8 @@ const darkTheme = createTheme({
   type: 'dark',
   theme: {
     colors: {
-      // brand colors
-      primaryLight: '$green200',
-      primaryLightHover: '$green300',
-      primaryLightActive: '$green400',
-      primaryLightContrast: '$green600',
-      primary: '#4ADE7B',
-      primaryBorder: '$green500',
-      primaryBorderHover: '$green600',
-      primarySolidHover: '$green700',
-      primarySolidContrast: '$white',
-      primaryShadow: '$green500',
-
-      gradient:
-        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
-      link: '#5E1DAD',
-
-      // you can also create your own color
-      myColor: '#ff4ecd',
-
-      // ...  more colors
-    },
-    space: {},
-    fonts: {},
+      myColor: '#000000',
+    }
   }, // optional
 });
 
